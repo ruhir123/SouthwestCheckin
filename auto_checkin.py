1a0b572b313c740c346d7cab1066fd2a67debd26
sparklesrainbow <30987196+sparklesrainbow@users.noreply.github.com>
2018-04-06 18:59:19 -0500

    Update auto_checkin.py

--- auto_checkin.py
+++ auto_checkin.py
@@ -63,15 +63,20 @@ def find_times(firstName, lastName, confirmation):
 
 
 
-def auto_checkin(firstName, lastName, confirmation, flightDateTime=None):
+def auto_checkin(firstName, lastName, confirmation, flightDateTime=None, phone = None):
 
 	baseUrl = 'https://www.southwest.com'
 	dataUrl = '/flight/retrieveCheckinDoc.html?firstName='+firstName+'&lastName='+lastName+'&confirmationNumber='+confirmation
 	checkinUrl = urlparse.urljoin(baseUrl, dataUrl)
 	print(checkinUrl)
 
-	waitTime = (flightDateTime - timedelta(days=1)) - utc.localize(datetime.utcnow())
+	waitTime = flightDateTime - utc.localize(datetime.utcnow())
 
+	if waitTime.total_seconds() < 0:
+		print "\n\n\nThis flight seems to have already left... Not checking in\n\n\n"
+		return
+
+	waitTime = waitTime - timedelta(days=1)
 	#print waitTime
 
 	wtime = waitTime.total_seconds()
@@ -96,31 +101,54 @@ def auto_checkin(firstName, lastName, confirmation, flightDateTime=None):
 
 	while loop:
 
-		err = browser.find_element_by_class_name('error-reservation-not-found')
-		if err:
-			print("Found Element Error ", err)
-			button = browser.find_element_by_class_name('submit-button')
-			button.click()
-			html = browser.page_source
-		else:
-			loop = False
-
 		print ("Sleeping 10 Seconds ...")
 		timeModule.sleep(10)
 
-	#browser.quit()
+		try: 
+			err = browser.find_element_by_class_name('error-reservation-not-found')
+			print("Found Element Error ", err)
+		except:
+			print("No Error Found: Success")
+			loop = False
+
+		button = browser.find_element_by_class_name('submit-button')
+		button.click()
+		html = browser.page_source
+
+	## Print boarding Position
+	#class: air-check-in-passenger-item--information-boarding-position
+	#sub-class: swa-g-screen-reader
+	#.text of that gets the "Boarding Position B59"
+	
+	#bp = browser.find_element_by_class_name('air-check-in-passenger-item--information-boarding-position')
+	#bpt = bp.find_element_by_class_name('swa-g-screen-reader-only')
+	#print ("\n\n\n *** Your Boarding Position is ", bpt.text)	
+	#print ("\n\n\n *** Your Boarding Position is ", bpt.get_attribute('value'))
+
+	## Text the boarding pass
+	if phone:
+		phBtn = browser.find_element_by_class_name('boarding-pass-options--button-text')
+		phBtn.click()
+		inp1 = browser.find_element_by_xpath('//*[@id="textBoardingPass"]')
+		inp1.click()
+		inp1.send_keys(phone)
+		timeModule.sleep(10)
+		#button = browser.find_element_by_class_name('submit-button')
+		button = browser.find_element_by_xpath('//*[@id="form-mixin--submit-button"]')
+		button.click()
+
+	browser.quit()
 
 	
 if __name__ == '__main__':
+
 	firstName = raw_input("First Name: ")
 	lastName = raw_input("Last Name: ")
 	confirmation = raw_input("Confirmation Number: ")
+	phone = raw_input("Phone Number: ")
 
 	flightinfo = find_times(firstName, lastName, confirmation)
 	for flight in flightinfo:
 		if is_dst(dictCode[flight[3]]):
 			flight[0] = flight[0] - timedelta(hours=1)		
-		auto_checkin(firstName, lastName, confirmation, flight[0])
-
-
-
+		auto_checkin(firstName, lastName, confirmation, flight[0], phone)
