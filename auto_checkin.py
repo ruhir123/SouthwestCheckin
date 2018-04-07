0293d01e55f980bda9938424b1c89ed1b5174eb0
sparklesrainbow <30987196+sparklesrainbow@users.noreply.github.com>
2018-04-06 19:01:11 -0500

    Update auto_checkin.py

--- auto_checkin.py
+++ auto_checkin.py
@@ -152,3 +152,4 @@ if __name__ == '__main__':
 		if is_dst(dictCode[flight[3]]):
 			flight[0] = flight[0] - timedelta(hours=1)		
 		auto_checkin(firstName, lastName, confirmation, flight[0], phone)
+		
