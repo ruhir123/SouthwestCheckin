353804384fd8ea43358eda5fa28b176a2596e051
sparklesrainbow <30987196+sparklesrainbow@users.noreply.github.com>
2018-11-25 21:31:21 -0600

    Update auto_checkin.py

--- auto_checkin.py
+++ auto_checkin.py
@@ -17,8 +17,8 @@ with open(fname) as f:
     content = f.read().splitlines()
 
 for line in content:
-	val = line.split('\t')
-	dictCode[val[0]] = val[1]
+   val = line.split('\t')
+   dictCode[val[0]] = val[1]
 
 #print dictCode
 utc = timezone('UTC')
@@ -33,123 +33,123 @@ def is_dst (tzone):
 
 
 def find_times(firstName, lastName, confirmation):
-	baseUrl = 'https://www.southwest.com'
-	infoUrl = '/flight/view-air-reservation.html?confirmationNumberFirstName='+firstName+'&confirmationNumberLastName='+lastName+'&confirmationNumber='+confirmation
-	retrieveUrl = urlparse.urljoin(baseUrl, infoUrl)
-	print(retrieveUrl)
+   baseUrl = 'https://www.southwest.com'
+   infoUrl = '/flight/view-air-reservation.html?confirmationNumberFirstName='+firstName+'&confirmationNumberLastName='+lastName+'&confirmationNumber='+confirmation
+   retrieveUrl = urlparse.urljoin(baseUrl, infoUrl)
+   print(retrieveUrl)
 
-	browser = webdriver.Firefox()
-	browser.get(retrieveUrl)
-	html = browser.page_source
-	timeModule.sleep(10)
+   browser = webdriver.Firefox()
+   browser.get(retrieveUrl)
+   html = browser.page_source
+   timeModule.sleep(10)
 
-	flightSegments  = browser.find_elements_by_class_name('checkout-flight-detail')
-	print("Found Segments ", len(flightSegments))
-	retval=[]
-	for fSegment in flightSegments:
-		fdate = fSegment.find_elements_by_class_name('flight-detail--heading-date')[0].text.split(' ')[0].split('/')
-		ftime = fSegment.find_elements_by_class_name('time--value')[0].text
-		ft = ftime[:-2].split(':')
-		if ftime[-2:] == "PM":
-			ft[0] = int(ft[0]) + 12
+   flightSegments  = browser.find_elements_by_class_name('checkout-flight-detail')
+   print("Found Segments ", len(flightSegments))
+   retval=[]
+   for fSegment in flightSegments:
+      fdate = fSegment.find_elements_by_class_name('flight-detail--heading-date')[0].text.split(' ')[0].split('/')
+      ftime = fSegment.find_elements_by_class_name('time--value')[0].text
+      ft = ftime[:-2].split(':')
+      if ftime[-2:] == "PM":
+         ft[0] = int(ft[0]) + 12
 
-		fCode = fSegment.find_elements_by_class_name('flight-segments--airport-code')[0].text
+      fCode = fSegment.find_elements_by_class_name('flight-segments--airport-code')[0].text
 
-		fDateTime = datetime(int('20' + fdate[2]), int(fdate[0]), int(fdate[1]), int(ft[0]), int(ft[1]), 0, 0, tzinfo=timezone(dictCode[fCode]))
+      fDateTime = datetime(int('20' + fdate[2]), int(fdate[0]), int(fdate[1]), int(ft[0]), int(ft[1]), 0, 0, tzinfo=timezone(dictCode[fCode]))
 
-		#print (fdate, ftime, fCode, fDateTime)
-		retval.append([fDateTime, fdate, ftime, fCode])
+      #print (fdate, ftime, fCode, fDateTime)
+      retval.append([fDateTime, fdate, ftime, fCode])
 
 
-	print retval
-	browser.quit()
-	return retval
+   print retval
+   browser.quit()
+   return retval
 
 
 
 def auto_checkin(firstName, lastName, confirmation, flightDateTime=None, phone = None):
 
-	baseUrl = 'https://www.southwest.com'
-	dataUrl = '/flight/retrieveCheckinDoc.html?firstName='+firstName+'&lastName='+lastName+'&confirmationNumber='+confirmation
-	checkinUrl = urlparse.urljoin(baseUrl, dataUrl)
-	print(checkinUrl)
+   baseUrl = 'https://www.southwest.com'
+   dataUrl = '/flight/retrieveCheckinDoc.html?firstName='+firstName+'&lastName='+lastName+'&confirmationNumber='+confirmation
+   checkinUrl = urlparse.urljoin(baseUrl, dataUrl)
+   print(checkinUrl)
 
-	waitTime = flightDateTime - utc.localize(datetime.utcnow())
+   waitTime = flightDateTime - utc.localize(datetime.utcnow())
 
-	if waitTime.total_seconds() < 0:
-		print "\n\n\nThis flight seems to have already left... Not checking in\n\n\n"
-		return
+   if waitTime.total_seconds() < 0:
+      print "\n\n\nThis flight seems to have already left... Not checking in\n\n\n"
+      return
 
-	#waitTime = waitTime - timedelta(days=1)
-	#print waitTime
+   #waitTime = waitTime - timedelta(days=1)
+   #print waitTime
 
-	#wtime = waitTime.total_seconds()
-	#print("Waiting for ", wtime, "time", waitTime)
+   #wtime = waitTime.total_seconds()
+   #print("Waiting for ", wtime, "time", waitTime)
 
-	# Launch Firefox GUI
-	checkinTime = flightDateTime - timedelta(days=1)
-	wtime = (checkinTime - utc.localize(datetime.utcnow())).total_seconds()
-	while (wtime > 10):
-		wtime = (checkinTime - utc.localize(datetime.utcnow())).total_seconds()
-		timeModule.sleep(10)
-		wdays = int(wtime/60/60/24)
-		whours = int((wtime - wdays*60*60*24)/60/60)
-		wmins = int((wtime - wdays*60*60*24 - whours*60*60)/60)
-		wsecs = int(wtime - wdays*60*60*24 - whours*60*60 - wmins*60)
-		print 'Waiting to check in for ',  wdays, " Days ", whours, " Hours ", wmins, " Minutes ", wsecs, " Seconds "
-		#timeModule.sleep(waitTime.total_seconds()-1)
+   # Launch Firefox GUI
+   checkinTime = flightDateTime - timedelta(days=1)
+   wtime = (checkinTime - utc.localize(datetime.utcnow())).total_seconds()
+   while (wtime > 10):
+      wtime = (checkinTime - utc.localize(datetime.utcnow())).total_seconds()
+      timeModule.sleep(10)
+      wdays = int(wtime/60/60/24)
+      whours = int((wtime - wdays*60*60*24)/60/60)
+      wmins = int((wtime - wdays*60*60*24 - whours*60*60)/60)
+      wsecs = int(wtime - wdays*60*60*24 - whours*60*60 - wmins*60)
+      print 'Waiting to check in for ',  wdays, " Days ", whours, " Hours ", wmins, " Minutes ", wsecs, " Seconds "
+      #timeModule.sleep(waitTime.total_seconds()-1)
 
-	print("Time to Checkin ...", checkinUrl)
+   print("Time to Checkin ...", checkinUrl)
 
-	loop = True
-	browser = webdriver.Firefox()
-	browser.get(checkinUrl)
-	html = browser.page_source
+   loop = True
+   browser = webdriver.Firefox()
+   browser.get(checkinUrl)
+   html = browser.page_source
 
-	while loop:
+   while loop:
 
-		print ("Sleeping 10 Seconds ...")
-		timeModule.sleep(10)
+      print ("Sleeping 10 Seconds ...")
+      timeModule.sleep(10)
 
-		try: 
-			err = browser.find_element_by_class_name('error-reservation-not-found')
-			print("Found Element Error ", err)
-		except:
-			print("No Error Found: Success at ", datetime.now())
-			loop = False
+      try: 
+         err = browser.find_element_by_class_name('error-reservation-not-found')
+         print("Found Element Error ", err)
+      except:
+         print("No Error Found: Success at ", datetime.now())
+         loop = False
 
-		button = browser.find_element_by_class_name('submit-button')
-		button.click()
-		timeModule.sleep(10)
-		html = browser.page_source
+      button = browser.find_element_by_class_name('submit-button')
+      button.click()
+      timeModule.sleep(10)
+      html = browser.page_source
 
-	## Print boarding Position
-	#class: air-check-in-passenger-item--information-boarding-position
-	#sub-class: swa-g-screen-reader
-	#.text of that gets the "Boarding Position B59"
-	
-	#bp = browser.find_element_by_class_name('air-check-in-passenger-item--information-boarding-position')
-	#bpt = bp.find_element_by_class_name('swa-g-screen-reader-only')
-	#print ("\n\n\n *** Your Boarding Position is ", bpt.text)	
-	#print ("\n\n\n *** Your Boarding Position is ", bpt.get_attribute('value'))
+   ## Print boarding Position
+   #class: air-check-in-passenger-item--information-boarding-position
+   #sub-class: swa-g-screen-reader
+   #.text of that gets the "Boarding Position B59"
+   
+   #bp = browser.find_element_by_class_name('air-check-in-passenger-item--information-boarding-position')
+   #bpt = bp.find_element_by_class_name('swa-g-screen-reader-only')
+   #print ("\n\n\n *** Your Boarding Position is ", bpt.text)   
+   #print ("\n\n\n *** Your Boarding Position is ", bpt.get_attribute('value'))
 
-	## Text the boarding pass
-	if phone:
-		try:
-			phBtn = browser.find_element_by_class_name('boarding-pass-options--button-text')
-			phBtn.click()
-			inp1 = browser.find_element_by_xpath('//*[@id="textBoardingPass"]')
-			inp1.click()
-			inp1.send_keys(phone)
-			timeModule.sleep(10)
-			#button = browser.find_element_by_class_name('submit-button')
-			button = browser.find_element_by_xpath('//*[@id="form-mixin--submit-button"]')
-			button.click()
-		except:
-			print("wierd error -- No Text For You -- ")
+   ## Text the boarding pass
+   if phone:
+      try:
+         phBtn = browser.find_element_by_class_name('boarding-pass-options--button-text')
+         phBtn.click()
+         inp1 = browser.find_element_by_xpath('//*[@id="textBoardingPass"]')
+         inp1.click()
+         inp1.send_keys(phone)
+         timeModule.sleep(10)
+         #button = browser.find_element_by_class_name('submit-button')
+         button = browser.find_element_by_xpath('//*[@id="form-mixin--submit-button"]')
+         button.click()
+      except:
+         print("wierd error -- No Text For You -- ")
 
-	timeModule.sleep(10)
-	browser.quit()
+   timeModule.sleep(10)
+   browser.quit()
 
 
 def main(argv):
@@ -181,31 +181,31 @@ def main(argv):
    time = 1000000
 
    if platform.system() == 'Darwin': # MACOS needs caffienate
-		t1 = Thread(target=doCheckIn,args=(firstName,lastName,confirmation,phone,))
-		t2 = Thread(target=runCaffeinate,args=(time,))
+      t1 = Thread(target=doCheckIn,args=(firstName,lastName,confirmation,phone,))
+      t2 = Thread(target=runCaffeinate,args=(time,))
 
-		t1.start()
-		t2.start()
+      t1.start()
+      t2.start()
 
-		print("Waiting to join")
-		t1.join()
-		t2.terminate()
-		print("Finished Thread1")
+      print("Waiting to join")
+      t1.join()
+      t2.terminate()
+      print("Finished Thread1")
 
    else:
-		doCheckIn(firstName,lastName,confirmation,phone)
+      doCheckIn(firstName,lastName,confirmation,phone)
 
 def runCaffeinate(time):
-	os.system('caffeinate -t 0')
+   os.system('caffeinate -t 0')
 
 def doCheckIn(firstName,lastName,confirmation,phone):
 
-	flightinfo = find_times(firstName, lastName, confirmation)
-	for flight in flightinfo:
-		if is_dst(dictCode[flight[3]]):
-			flight[0] = flight[0] - timedelta(hours=1)		
-		auto_checkin(firstName, lastName, confirmation, flight[0], phone)
+   flightinfo = find_times(firstName, lastName, confirmation)
+   for flight in flightinfo:
+      if is_dst(dictCode[flight[3]]):
+         flight[0] = flight[0] - timedelta(hours=1)      
+      auto_checkin(firstName, lastName, confirmation, flight[0], phone)
 
-	
+   
 if __name__ == '__main__':
-	main(sys.argv[1:])
+   main(sys.argv[1:])
